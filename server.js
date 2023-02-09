3638da0625c79f063d42152ac6cf9868ed46ff15
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2023-02-09 16:51:32 +0800

    Adding comments and testing delete route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
+// To create unique IDs
 const { v4: uuidv4 } = require('uuid');
 
 var dbData = require('./db/db.json');
@@ -9,21 +10,26 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
+// GET Route for notes page
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// GET Route for retrieving all the tips
 app.get("/api/notes", (req, res) => res.json(dbData));
 
+// GET Route for always redirecting to index homepage
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
+// POST Route for adding a new note
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     if (!newNote.title ||!newNote.text) {
@@ -37,6 +43,7 @@ app.post("/api/notes", (req, res) => {
     }
 });
 
+// Delete Route for deleting a specific note with a specific id
 app.delete("/api/notes/:id", (req, res) => {
     let id = req.params.id;
     dbData = dbData.filter(note => note.id!== id);
